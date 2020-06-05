33e6926ecf92cc7bcbc6c385e4c257a006359ffd
Văn Thông Lê <thonglevan131@gmail.com>
2020-06-05 21:13:34 +0700

    Updates

---
+++ precache-manifest.ee83ca225a5adf830c94b84bb40fb66e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6fc4bc94cfef2804feecef00353448c",
+    "url": "/robot-finder/index.html"
+  },
+  {
+    "revision": "1b6b4c3841231aec4967",
+    "url": "/robot-finder/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/robot-finder/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "1b6b4c3841231aec4967",
+    "url": "/robot-finder/static/js/main.827d52e8.chunk.js"
+  },
+  {
+    "revision": "b2a0c193406974505e94",
+    "url": "/robot-finder/static/js/runtime~main.adaf2559.js"
+  }
+]);
